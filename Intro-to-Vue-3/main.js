0aa0dba274563b220a6f2e97a1f12cf476253bc3
Begench Bekmyradov <47065940+bekmuradov@users.noreply.github.com>
2020-09-18 15:13:28 +0700

    Add files via upload

---
+++ Intro-to-Vue-3/main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aspernatur quasi, possimus nulla corporis quod tenetur. Veritatis debitis fugiat dolores quod, asperiores expedita maiores labore culpa reiciendis repudiandae optio omnis?'
+        }
+    }
+})
